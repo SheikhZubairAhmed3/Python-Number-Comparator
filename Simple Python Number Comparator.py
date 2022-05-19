0ffff857ee5b20a4676cb5c6f8c525ff7cad583e
SheikhZubairAhmed3 <105794849+SheikhZubairAhmed3@users.noreply.github.com>
2022-05-19 22:06:30 +0530

    Add files via upload
    This is code for a simple Python Number Comparator
    This is a very simple and small code, only 4KB
    If you like my code please follow me
    Note:It does not have a GUI

---
+++ Simple Python Number Comparator.py
@@ -0,0 +1,26 @@
+#==============================================================================#
+#Made By : Zubair.
+#Greater Than,Less Than or Equal To Checker.
+#==============================================================================#
+#Put in the numbers in a and b then save the code and run.
+#You will get the output in the Terminal
+#==============================================================================#
+
+a = 12
+b = 5756
+
+def myfunc():
+   global a
+   global b
+
+if a > b:
+  print("a is greater than b")
+elif a==b:
+  print("a is equal to b")
+else:
+  print("b is greater than a")
+
+myfunc()
+
+
+
